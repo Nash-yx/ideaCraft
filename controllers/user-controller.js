276117d0faac52a59780deffc36d73b11bd6ae6e
Nash <nash.git1217@gmail.com>
2025-07-22 16:39:52 +0800

    feat: modify layouts

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,7 @@
 const userServices = require('../services/user-services')
 const userController = {
   signupPage: (req, res) => {
-    res.render('signup')
+    res.render('signup', { layout: 'auth' })
   },
   signup: async (req, res, next) => {
     try {
@@ -13,16 +13,20 @@ const userController = {
     }
   },
   loginPage: (req, res) => {
-    res.render('login')
+    res.render('login', { layout: 'auth' })
   },
   login: (req, res) => {
     req.flash('success_msg', 'Login successfully')
     return res.redirect('/')
   },
-  logout: (req, res) => {
+  logout: (req, res, next) => {
     req.flash('success_msg', 'Logout successfully')
-    req.logout()
-    return res.redirect('/login')
+    req.logout((err) => {
+      if (err) {
+        return next(err)
+      }
+      return res.redirect('/login')
+    })
   }
 }
 
